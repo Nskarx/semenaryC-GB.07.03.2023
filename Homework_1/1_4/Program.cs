afbcab457d606f68a8ea0ce595617b2369d35b97
Evgeniy <Lymilight@gmail.com>
2023-03-09 22:09:11 +0700

    hw4

--- Homework_1/1_4/Program.cs
+++ Homework_1/1_4/Program.cs
@@ -3,4 +3,16 @@
 // чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
- 
+ Console.Write("please enter N number : ");
+int n = int.Parse(Console.ReadLine()!);
+int i=1;
+ while(i <= n)             
+{
+    
+     i++;
+     if( i % 2 == 0)
+     {
+          Console.Write( i + ",");  
+     }
+}
+
