594b6b03f3dc0545a3cce99911130568f5531ae2
Evgeniy <Lymilight@gmail.com>
2023-03-10 14:44:52 +0700

    tast 2 les2

--- Lesson_2/2_0/Program.cs
+++ Lesson_2/2_0/Program.cs
@@ -4,13 +4,13 @@
 // 456 -> 6
 // 782 -> 2
 // 918 -> 8
-int TakeNum( int num);
-{
-     Console.WriteLine(num);
-   return num % 10 ;
-    Console.WriteLine(num);
-}
+// int TakeNum( int num);
+// {
+//      Console.WriteLine(num);
+//    return num % 10 ;
+//     Console.WriteLine(num);
+// }
 
-int th_num = new Random().Next(100, 1000);
- int result = TakeNum(th_num);
- Console.WriteLine(result);
+// int th_num = new Random().Next(100, 1000);
+//  int result = TakeNum(th_num);
+//  Console.WriteLine(result);
