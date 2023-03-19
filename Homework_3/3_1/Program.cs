1d72776d2e283614ec74bce3575fedb4698894c4
Evgeniy <Lymilight@gmail.com>
2023-03-19 15:06:56 +0700

    task 4_0

--- Homework_3/3_1/Program.cs
+++ Homework_3/3_1/Program.cs
@@ -8,19 +8,35 @@
 
 // 23432 -> да
 
-Console.Write("Введите пятизначное число: ");
-string number =  Console.ReadLine();
+// Console.Write("Введите пятизначное число: ");
+// string number =  Console.ReadLine();
 
-void CheckingNumber(string number)
-{
-  if (number[0]==number[4] || number[1]==number[3])
-  {
-    Console.WriteLine($"Число: {number} - палиндром.");
-  }
-  else Console.WriteLine($"Число: {number} - не палиндром.");
-}
-if (number!.Length == 5)
-{
-  CheckingNumber(number);
-}
-else Console.WriteLine($"Введи правильное число");
+// void CheckingNumber(string number)
+// {
+//   if (number[0]==number[4] || number[1]==number[3])
+//   {
+//     Console.WriteLine($"Число: {number} - палиндром.");
+//   }
+//   else Console.WriteLine($"Число: {number} - не палиндром.");
+// }
+// if (number!.Length == 5)
+// {
+//   CheckingNumber(number);
+// }
+// else Console.WriteLine($"Введи правильное число");
+
+
+    // решение преподователя
+     void Pali(int num)
+     {
+      int num_1_2 = num / 1000;
+      int num_5 = num % 10;
+      int num_4 =num / 10 % 10;
+      if(num_1_2 == num_5 * 10 + num_4)
+      Console.WriteLine($"yes, { num} is a palindrome");
+      else
+      Console.WriteLine($"no, {num} in not palindrome");
+     }
+     Console.WriteLine("enter a five -digit number");
+     int val = int.Parse(Console.ReadLine()!);
+     Pali(val);
---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+// 7 ->28
+// 4->10
+// 8 -> 36
+
+int SumNum(int num)
+{
+    int sum_all =0;
+   for(int i = 1; i <= num; i++ ) 
+   sum_all +=i;
+   return  sum_all;
+}
+ int val = int.Parse(Console.ReadLine()!);
+ int result = SumNum(val);
+ Console.WriteLine(result);
