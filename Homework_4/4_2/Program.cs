71a163ba6e81a83f7f03d882ef249f96e1a5b1aa
Evgeniy <Lymilight@gmail.com>
2023-03-19 16:23:45 +0700

    HW 4_2

---
+++ Homework_4/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+int sum = 0;
+foreach (char digit in n.ToString())
+{
+    int num = int.Parse(digit.ToString());
+    sum += num;
+}
+Console.WriteLine("Sum of digits: " + sum);
