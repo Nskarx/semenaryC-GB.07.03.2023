d023a6f471e687e8da28e8f6eef6f3c3973ae4d8
Evgeniy <Lymilight@gmail.com>
2023-03-24 17:30:22 +0700

    HW5_3

---
+++ Homework_5/5_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным
+// элементов массива.
+// [3, 7, 22, 2, 78] -> 76
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        double[] arr = { 3.0, 7.0, 22.0, 2.0, 78.0 }; // инициализация массива
+        double max = arr[0], min = arr[0];
+
+        // поиск максимального и минимального элементов
+        for (int i = 1; i < arr.Length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        double diff = max - min;
+        Console.WriteLine("Разница между максимальным и минимальным элементами: " + diff);
+    }
+}
