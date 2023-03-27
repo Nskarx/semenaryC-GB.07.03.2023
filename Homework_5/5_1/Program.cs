5be1884112d58b95dc9ae0a9ed3f7620be8d21b4
Evgeniy <Lymilight@gmail.com>
2023-03-27 17:33:01 +0700

    HW5_repodovatel

--- Homework_5/5_1/Program.cs
+++ Homework_5/5_1/Program.cs
@@ -4,35 +4,74 @@
 // массиве.
 // [345, 897, 568, 234] -> 2
 
-using System;
+// using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] arr = new int[4]; // создаем массив на 4 элемента
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int[] arr = new int[4]; // создаем массив на 4 элемента
 
-        Random rnd = new Random(); // создаем объект для генерации случайных чисел
+//         Random rnd = new Random(); // создаем объект для генерации случайных чисел
 
-        // заполняем массив случайными положительными трехзначными числами
-        for (int i = 0; i < arr.Length; i++)
-        {
-            arr[i] = rnd.Next(100, 1000); // генерируем число от 100 до 999
-            Console.Write(arr[i] + " "); // выводим элементы массива на экран
-        }
+//         // заполняем массив случайными положительными трехзначными числами
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//             arr[i] = rnd.Next(100, 1000); // генерируем число от 100 до 999
+//             Console.Write(arr[i] + " "); // выводим элементы массива на экран
+//         }
 
-        // подсчет количества четных чисел в массиве
-        int evenCount = 0;
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] % 2 == 0) // если число четное
-            {
-                evenCount++; // увеличиваем счетчик четных чисел
-            }
-        }
+//         // подсчет количества четных чисел в массиве
+//         int evenCount = 0;
+//         for (int i = 0; i < arr.Length; i++)
+//         {
+//             if (arr[i] % 2 == 0) // если число четное
+//             {
+//                 evenCount++; // увеличиваем счетчик четных чисел
+//             }
+//         }
 
-        Console.WriteLine("\nКоличество четных чисел в массиве: " + evenCount);
+//         Console.WriteLine("\nКоличество четных чисел в массиве: " + evenCount);
         
-    }
+//     }
+// }
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)    
+        Console.Write($"{arr[i]} ");
+    
+    Console.WriteLine();
 }
 
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)    
+        arr[i] = new Random().Next(from, to + 1);
+    
+    return arr;
+}
+
+int EvenNum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)        
+            count += 1;                
+    }
+    return count;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Console.WriteLine(EvenNum(mass));
--- Homework_5/5_2/Program.cs
+++ Homework_5/5_2/Program.cs
@@ -3,26 +3,64 @@
 // на нечётных позициях.
 // [3, 7, 23, 12] -> 26
 // [-4, -6, 4, 67] -> 0
-using System;
+// using System;
 
-class Program {
-    static void Main(string[] args) {
-        // Создаем генератор случайных чисел
-        Random rand = new Random();
+// class Program {
+//     static void Main(string[] args) {
+//         // Создаем генератор случайных чисел
+//         Random rand = new Random();
 
-        // Создаем массив и заполняем его случайными числами
-        int[] arr = new int[10];
-        for (int i = 0; i < arr.Length; i++) {
-            arr[i] = rand.Next(0, 100);
-        }
+//         // Создаем массив и заполняем его случайными числами
+//         int[] arr = new int[10];
+//         for (int i = 0; i < arr.Length; i++) {
+//             arr[i] = rand.Next(0, 100);
+//         }
 
-        // Находим сумму элементов на нечетных позициях
-        int sum = 0;
-        for (int i = 1; i < arr.Length; i += 2) {
-            sum += arr[i];
-        }
+//         // Находим сумму элементов на нечетных позициях
+//         int sum = 0;
+//         for (int i = 1; i < arr.Length; i += 2) {
+//             sum += arr[i];
+//         }
 
-        // Выводим результат на экран
-        Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
-    }
+//         // Выводим результат на экран
+//         Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
+//     }
+// }
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)    
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
 }
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 1; i < size; i++)    
+        arr[i] = new Random().Next(from, to + 1);
+    
+    return arr;
+}
+
+int OddNumSum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i += 2)    
+        count += arr[i];
+    
+    return count;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+Console.WriteLine(OddNumSum(mass));
