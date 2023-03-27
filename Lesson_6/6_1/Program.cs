40134505e6e46de6bf96f76372b724420789eeb2
Evgeniy <Lymilight@gmail.com>
2023-03-27 18:00:43 +0700

    task6_2

--- Lesson_6/6_1/Program.cs
+++ Lesson_6/6_1/Program.cs
@@ -2,16 +2,34 @@
 //    и проверяет, может ли существовать треугольник с сторонами такой длины.
 
 
-void ExiTri(int n_1, int n_2, int n_3)
-{
-    if (n_1 < n_2 + n_3 && n_2 < n_1 + n_3 && n_3 < n_2 + n_1)
-        Console.WriteLine("The triangle exists.");
-    else
-        Console.WriteLine("The triangle does not exist.");
-}
+// void ExiTri(int n_1, int n_2, int n_3)
+// {
+//     if (n_1 < n_2 + n_3 && n_2 < n_1 + n_3 && n_3 < n_2 + n_1)
+//         Console.WriteLine("The triangle exists.");
+//     else
+//         Console.WriteLine("The triangle does not exist.");
+// }
 
-// 1 2 3, 4 2 5
-int num_1 = int.Parse(Console.ReadLine()!); 
-int num_2 = int.Parse(Console.ReadLine()!);
-int num_3 = int.Parse(Console.ReadLine()!);  
-ExiTri(num_1, num_2, num_3);
+// // 1 2 3, 4 2 5
+// int num_1 = int.Parse(Console.ReadLine()!); 
+// int num_2 = int.Parse(Console.ReadLine()!);
+// int num_3 = int.Parse(Console.ReadLine()!);  
+// ExiTri(num_1, num_2, num_3);
+
+
+// Console.Write("a=");
+// 	int a = int.Parse(Console.ReadLine()!);
+// 	Console.Write("b=");
+// 	int b = int.Parse(Console.ReadLine()!);
+// 	Console.Write("c=");
+// 	int c = int.Parse(Console.ReadLine()!);
+	
+// 	bool MyFunc(int a, int b, int c)
+// 	{
+// 	    if (a < b + c && b < a + c && c < a + b)
+// 	    {
+// 	        return true;
+// 	    } else return false;
+// 	}
+	
+// 	Console.Write(MyFunc(a,b,c));
---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,43 @@
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+
+// string Binary(int num)
+// {
+//     string res = "";
+
+//     while (num > 0)
+//     {
+//         res = num % 2 + res;
+//         num /= 2;
+//     }
+//     return res;
+// }
+
+// // 12 - 1100, 4 - 100, 45 - 101101
+// int n = int.Parse(Console.ReadLine()!);
+// string result = Binary(n);
+// Console.WriteLine(result);
+
+int num = GetUserNumber("number");
+	Console.WriteLine(MakeBinary(num));
+	
+	int GetUserNumber(string name)
+	{
+	    int number = 0;
+	    Console.Write($"Please enter {name}: ");
+	    number = int.Parse(Console.ReadLine()!);
+	    return number;
+	}
+	
+	string MakeBinary(int number)
+	{
+	    string binary = "";
+	
+	    while (number > 0)
+	    {
+	        binary = number%2 + binary;
+	        number /= 2;
+	    }
+	    return binary;
+	}
